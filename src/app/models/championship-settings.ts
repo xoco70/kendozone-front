87bece6786141a61c46388ce38e0bb396aee6142
Julien Cappiello <contact@kendozone.com>
2018-06-24 20:51:24 +0200

    add default settings via constructur

--- src/app/models/championship-settings.ts
+++ src/app/models/championship-settings.ts
@@ -18,4 +18,23 @@ export class ChampionshipSettings {
   hanteiLimit: number;
   enchoGoldPoint: number;
   limitByEntity: number;
+
+  constructor() {
+    this.fightingAreas = 1;
+    this.fightDuration = '05:00';
+    this.hasPreliminary = true;
+    this.preliminaryGroupSize = 3;
+    this.preliminaryDuration = '05:00';
+    this.preliminaryWinner = 1;
+    this.hasEncho = true;
+    this.enchoQty = 1;
+    this.enchoDuration = '0';
+    this.hasHantei = false;
+    this.hanteiLimit = 0;
+    this.enchoGoldPoint = 0;
+    this.limitByEntity = 4;
+    this.cost = 0;
+    this.treeType = 1;
+    this.seedQuantity = 4;
+  }
 }
