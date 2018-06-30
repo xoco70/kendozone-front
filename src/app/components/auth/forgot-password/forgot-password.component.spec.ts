705614467750c20281147436ee3729992fcd82b9
Julien Cappiello <contact@kendozone.com>
2018-06-30 20:32:02 +0200

    add forgot passsord spec

--- src/app/components/auth/forgot-password/forgot-password.component.spec.ts
+++ src/app/components/auth/forgot-password/forgot-password.component.spec.ts
@@ -1,19 +1,38 @@
 import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {ForgotPasswordComponent} from './forgot-password.component';
+import {TranslateModule} from '@ngx-translate/core';
+import {FormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import {Router} from '@angular/router';
 import {CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
+import {ToastrModule, ToastrService} from 'ngx-toastr';
+
+
+class MockRouter {
+  navigate = jasmine.createSpy('navigate');
+}
 
 describe('ForgotPasswordComponent', () => {
   let component: ForgotPasswordComponent;
   let fixture: ComponentFixture<ForgotPasswordComponent>;
 
   beforeEach(async(() => {
+    const mockRouter = new MockRouter();
     TestBed.configureTestingModule({
-      declarations: [ ForgotPasswordComponent ],
-      schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
-
+      imports: [
+        ToastrModule.forRoot(),
+        TranslateModule.forRoot(),
+        FormsModule,
+        HttpClientModule],
+      declarations: [ForgotPasswordComponent],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      providers: [
+        {provide: Router, useClass: MockRouter},
+        ToastrService,
+      ]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
@@ -22,7 +41,7 @@ describe('ForgotPasswordComponent', () => {
     fixture.detectChanges();
   });
 
-  // it('should create', () => {
-  //   expect(component).toBeTruthy();
-  // });
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
 });
