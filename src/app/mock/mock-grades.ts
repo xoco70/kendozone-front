f27b20fa233f416d257fc8f97e078701953d94fc
Julien Cappiello <contact@kendozone.com>
2018-07-14 12:32:48 +0200

    add grades constant

--- src/app/mock/mock-grades.ts
+++ src/app/mock/mock-grades.ts
@@ -82,3 +82,86 @@ export const GRADES: Grade[] = [
     'order': 15,
   }
 ];
+
+export const GRADES_PROFILE: Grade[] = [
+  {
+    'id': 1,
+    'text': ' - ',
+    'order': 0,
+  },
+  {
+    'id': 2,
+    'text': '7 Kyu',
+    'order': 1,
+  },
+  {
+    'id': 3,
+    'text': '6 Kyu',
+    'order': 2,
+  },
+  {
+    'id': 4,
+    'text': '5 Kyu',
+    'order': 3,
+  },
+  {
+    'id': 5,
+    'text': '4 Kyu',
+    'order': 4,
+  },
+  {
+    'id': 6,
+    'text': '3 Kyu',
+    'order': 5,
+  },
+  {
+    'id': 7,
+    'text': '2 Kyu',
+    'order': 6,
+  },
+  {
+    'id': 8,
+    'text': '1 Kyu',
+    'order': 7,
+  },
+  {
+    'id': 9,
+    'text': '1 Dan',
+    'order': 8,
+  },
+  {
+    'id': 10,
+    'text': '2 Dan',
+    'order': 9,
+  },
+  {
+    'id': 11,
+    'text': '3 Dan',
+    'order': 10,
+  },
+  {
+    'id': 12,
+    'text': '4 Dan',
+    'order': 11,
+  },
+  {
+    'id': 13,
+    'text': '5 Dan',
+    'order': 12,
+  },
+  {
+    'id': 14,
+    'text': '6 Dan',
+    'order': 13,
+  },
+  {
+    'id': 15,
+    'text': '7 Dan',
+    'order': 14,
+  },
+  {
+    'id': 16,
+    'text': '8 Dan',
+    'order': 15,
+  }
+];
