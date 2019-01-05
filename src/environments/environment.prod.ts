e96665cd03d952953812f2882b89b8c8d397e6bc
Cappiello Julien <julien@cappiello.fr>
2019-01-05 16:32:42 +0100

    add s3 url for production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   apiUrl: 'https://api.kendozone.com',
+  s3UrlBase: 'https://s3.amazonaws.com/kz-kendozone-v2',
   sentryDns: 'https://1a27df946e9742edaa7a4e9a2b41b280@sentry.io/1232263',
 };
