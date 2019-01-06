03d4af7a233f47222c4459910a0b64abc61dd189
Cappiello Julien <julien@cappiello.fr>
2019-01-06 14:33:28 +0100

    get Facebook authentication work

--- src/app/components/auth/login/login.component.ts
+++ src/app/components/auth/login/login.component.ts
@@ -7,6 +7,8 @@ import {AuthenticationService} from '../../../services/authentication.service';
 import {ToastrService} from 'ngx-toastr';
 import {NavService} from '../../../services/nav.service';
 
+declare var FB: any;
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -42,6 +44,27 @@ export class LoginComponent implements OnInit, AfterViewInit {
     // }
     // get return url from route parameters or default to '/'
     this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/dashboard';
+
+    (window as any).fbAsyncInit = function () {
+      FB.init({
+        appId: '797477373695958',
+        cookie: true,
+        xfbml: true,
+        version: 'v3.1'
+      });
+      FB.AppEvents.logPageView();
+    };
+
+    (function (d, s, id) {
+      let js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) {
+        return;
+      }
+      js = d.createElement(s);
+      js.id = id;
+      js.src = 'https://connect.facebook.net/en_US/sdk.js';
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
   }
 
   // convenience getter for easy access to form fields
@@ -76,4 +99,31 @@ export class LoginComponent implements OnInit, AfterViewInit {
       this.toastr.success('Register Successful, please login');
     }
   }
+
+  fbLogin() {
+    FB.login((response) => {
+      if (response.authResponse) {
+        this.getUserInfo(response.authResponse.userID, response.authResponse.accessToken);
+
+      } else {
+        this.toastr.error('Could not login with facebook');
+        console.log(response.message);
+      }
+    });
+  }
+
+  getUserInfo(userId, accessToken) {
+    FB.api(
+      '/' + userId + '?fields=id,name,first_name,email,picture',
+      (result) => {
+        console.log('result===', result);
+        if (result && !result.error) {
+        }
+      });
+  }
+
+  googleLogin() {
+
+  }
+
 }
